6dd6504ac351b31ea507604f332fe4e0b77a5504
RonaldLommerss <ronald.lommers@hva.nl>
2026-03-12 14:22:03 +0100

    Login Front-End geimplementeerd

--- server.js
+++ server.js
@@ -67,6 +67,8 @@ app.get('/', (req, res) => { res.render('index') })
 app.get('/profile', (req, res) => { res.render(`profile`) })
 
 app.get('/register', (req, res) => { res.render(`register`) })
+
+app.get('/login', (req, res) => { res.render(`login`) })
  
  
 app.listen(port, () => {
