73f26603ca2ad8fecafe9ee25ceef66fef6a53da
nathan <Nathan.v.munster@gmail.com>
2026-03-19 09:44:32 +0100

    pushhhhh

--- server.js
+++ server.js
@@ -78,6 +78,7 @@ app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 app.use('/static', express.static(path.join(__dirname, 'static')))
 app.use(express.static("static"));
+app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }))
  
 
